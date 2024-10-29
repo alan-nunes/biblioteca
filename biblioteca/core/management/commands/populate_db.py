a99f121807bc8b04f542fdc3d410053f51a30281
alan-nunes <201911250008@ifba.edu.br>
2024-10-29 20:42:04 -0300

    create owner

--- biblioteca/core/management/commands/populate_db.py
+++ biblioteca/core/management/commands/populate_db.py
@@ -1,10 +1,17 @@
 from django.core.management.base import BaseCommand
 from core.models import Categoria, Autor, Livro
+from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     help = "Cria registros de exemplo no banco de dados"
     
     def handle(self, *args, **options):
+        
+        owner = User.objects.first()
+        if not owner:
+            owner = User.objects.create(username="admin", password="admin")
+        
+        
         categoria_misterio = Categoria.objects.create(nome="Mistério")
         categoria_ficcao = Categoria.objects.create(nome="Ficção")
         categoria_fantasia = Categoria.objects.create(nome="Fantasia")
@@ -22,89 +29,104 @@ class Command(BaseCommand):
                 titulo="Assassinato no Expresso do Oriente",
                 autor=autor_agatha_christie,
                 categoria=categoria_misterio,
-                publicado_em="1934-01-01",)
- 
+                publicado_em="1934-01-01",
+                owner= owner)
+                
         Livro.objects.create(
             titulo="Morte no Nilo",
             autor=autor_agatha_christie,
             categoria=categoria_misterio,
-            publicado_em="1937-11-01",)
+            publicado_em="1937-11-01",
+            owner=owner)
         
         Livro.objects.create(
             titulo="2001: Uma Odisseia no Espaço",
             autor=autor_arthur_c_clarke,
             categoria=categoria_ficcao,
-            publicado_em="1968-06-16",)
+            publicado_em="1968-06-16",
+            owner=owner)
         
         Livro.objects.create(
             titulo="Encontro com Rama",
             autor=autor_arthur_c_clarke,
             categoria=categoria_ficcao,
-            publicado_em="1973-06-01",)
+            publicado_em="1973-06-01",
+            owner=owner)
         
         Livro.objects.create(
             titulo="O Cão dos Baskervilles",
             autor=autor_arthur_conan_doyle,
             categoria=categoria_misterio,
-            publicado_em="1902-04-01",)
+            publicado_em="1902-04-01",
+            owner=owner)
         
         Livro.objects.create(
             titulo="Um Estudo em Vermelho",
             autor=autor_arthur_conan_doyle,
             categoria=categoria_misterio,
-            publicado_em="1887-11-01",)
+            publicado_em="1887-11-01",
+            owner=owner)
         
         Livro.objects.create(
             titulo="As Crônicas de Nárnia",
             autor=autor_cs_lewis,
             categoria=categoria_fantasia,
-            publicado_em="1950-10-16",)
+            publicado_em="1950-10-16",
+            owner=owner)
         
         Livro.objects.create(
             titulo="O Leão, a Feiticeira e o Guarda-Roupa",
             autor=autor_cs_lewis,
             categoria=categoria_fantasia,
-            publicado_em="1950-10-16",)
+            publicado_em="1950-10-16",
+            owner=owner)
         
         Livro.objects.create(
             titulo="O Morro dos Ventos Uivantes",
             autor=autor_emily_bronte,
             categoria=categoria_romance,
-            publicado_em="1847-12-01",)
+            publicado_em="1847-12-01",
+            owner=owner)
         
         Livro.objects.create(
             titulo="A Guerra dos Tronos",
             autor=autor_george_rr_martin,
             categoria=categoria_fantasia,
-            publicado_em="1996-08-06",)
+            publicado_em="1996-08-06",
+            owner=owner)
         
         Livro.objects.create(
             titulo="A Fúria dos Reis",
             autor=autor_george_rr_martin,
             categoria=categoria_fantasia,
-            publicado_em="1998-11-16",)
+            publicado_em="1998-11-16",
+            owner=owner)
         
         Livro.objects.create(
             titulo="Fundação",
             autor=autor_isaac_asimov,
             categoria=categoria_ficcao,
-            publicado_em="1951-06-01",)
+            publicado_em="1951-06-01",
+            owner=owner)
         
         Livro.objects.create(
             titulo="Eu, Robô",
             autor=autor_isaac_asimov,
             categoria=categoria_ficcao,
-            publicado_em="1950-12-02",)
+            publicado_em="1950-12-02",
+            owner=owner)
         
         Livro.objects.create(
             titulo="O Senhor dos Anéis",
             autor=autor_jrr_tolkien,
             categoria=categoria_fantasia,
-            publicado_em="1954-07-29",)
+            publicado_em="1954-07-29",
+            owner=owner)
         
         Livro.objects.create(
             titulo="O Hobbit",
             autor=autor_jrr_tolkien,
             categoria=categoria_fantasia,
-            publicado_em="1937-09-21",)
+            publicado_em="1937-09-21",
+            owner=owner)
     
